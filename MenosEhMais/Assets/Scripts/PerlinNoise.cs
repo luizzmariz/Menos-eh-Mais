d5ed70df24f7b0eedfb839c2012841b613452fe7
Heyse <luizedumariz@gmail.com>
2023-09-23 02:02:39 -0300

    Mariz 2

---
+++ MenosEhMais/Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256; 
+
+    public float scale = 20f;
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.mainTexture = GenerateTexture();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+
+        return new Color(sample, sample, sample);
+    }
+}
